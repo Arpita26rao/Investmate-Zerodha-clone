400fe811ec4b5a894c4f94ac20efd775984a9ef3
Arpita rao <raoarpita316@gmail.com>
2026-06-30 20:33:56 +0530

    Fix routing structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./home/Homepage";
+import AboutPage from "./landing_Page/About/AboutPage";
+import Signup from "./signup/Signup";
+import SupportPage from "./support/SupportPage";
+import ProductPage from "./products/ProductPage";
+import PricingPage from "./pricing/PricingPage";
+import Holdings from "./Holdings/Holdings";
+
 function App() {
-  return <h1>HELLO FRONTEND</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/holdings" element={<Holdings />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/support" element={<SupportPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.js";
-
-import HomePage from "./home/Homepage.js";
-import AboutPage from "./landing_Page/About/AboutPage.js";
-import Signup from "./signup/Signup.js";
-import SupportPage from "./support/SupportPage.js";
-import ProductPage from "./products/ProductPage.js";
-import PricingPage from "./pricing/PricingPage.js";
-import Holdings from "./Holdings/Holdings.js";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/Holdings" element={<Holdings />} />
-      <Route path="/product" element={<ProductPage />} />
-      <Route path="/support" element={<SupportPage />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
